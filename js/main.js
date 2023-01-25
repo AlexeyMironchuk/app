45afa0c1bacc1135ab591b8aa0ba89e58e1c3b35
AlexeyMironchuk <oleksiimironchuk@gmail.com>
2023-01-25 18:02:55 +0200

    new attempt

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function calcSumm(numOne, numTwo) {
+    console.log(`Переменная numOne:${numOne}`);
+    console.log(`Переменная numTwo:${numTwo}`);
+
+    let numSumm = numOne + numTwo;
+
+    console.log(`Сумма:${numSumm}`);
+}
+
+calcSum(1,2);
